9c6d82885544fc5d54d548ccaa4f0cfdb98a5d5b
Steve Schnepp <steve.schnepp@pwkf.org>
2018-05-07 17:04:38 +0200

    inti

---
+++ rtbtrace.pl
@@ -0,0 +1,106 @@
+#! /usr/bin/perl
+# Copyright (C) 2018 - Steve Schnepp - Apache2
+
+use strict;
+use warnings;
+	
+# Give the blocksize as first arg
+my $nb_sectors = shift;
+if (! $nb_sectors) {
+	die "Should give the number of 512b sectors as first arg";
+}
+
+my $fileno = 0;
+my $tstp_step = 1 / 25; # 25 Hz
+my $last_tstp = 0;
+
+
+my @io_ops = ();
+while(1) {
+	my $line = <>;
+	chomp($line);
+	# 8,0    3        1     0.000000000   697  G   W 223490 + 8 [kjournald]
+	my ($device, $cpu_id, $seqno, $tstp, $pid, $action, $mode, $offset, $dummy, $length, $detail) = split(/ +/, trim($line), 11);
+
+	# Only take complete lines
+	next unless $detail;
+
+	# Only take the C (completed) requests to take care of an eventual buffering/queuing
+	next unless $action eq 'C';
+
+	# Flush if needed. Assumes the data is timestamp ordered
+	if ($tstp > $last_tstp + $tstp_step) {
+		$last_tstp += $tstp_step;
+
+		# flush to img
+		draw(\@io_ops);
+
+		# flush the in-flight IO ops
+		@io_ops = ();
+	}
+
+	# Fill the in-flight IO ops
+	push @io_ops, [ $offset, $mode, $length ];
+} continue {
+}
+
+sub draw 
+{
+	my ($io_ops) = @_;
+
+	my $columns = 640;
+	my $rows = 480;
+	my $kb_per_pixels = $nb_sectors / ($columns * $rows) / 2;
+
+	use GD;
+
+	my $img = new GD::Image($columns, $rows);
+	# allocate some colors
+	my $black = $img->colorAllocate(0,0,0); # First color, also background
+	my $white = $img->colorAllocate(255,255,255);
+	my $red = $img->colorAllocate(255,0,0);    
+	my $blue = $img->colorAllocate(0,0,255);
+	my $green = $img->colorAllocate(0,255,0);
+
+	# Update the status line
+	my $last_tstp_as_string = sprintf("%.2f", $last_tstp);
+	$img->string(gdSmallFont, 0, 0, "t: $last_tstp_as_string", $white);
+
+	# Iterate & fill the window
+	for my $io_op (@$io_ops) {
+		my ($offset_in_kb, $mode, $length) = @$io_op;
+
+		my $offset_in_pixels = $offset_in_kb / $kb_per_pixels;
+		my $x = int ($offset_in_pixels / $columns);
+		my $y = int ($offset_in_pixels % $columns);
+
+		# Green = read, Red = Write
+		my $color = ($mode =~ m/R/ ? $green : $red);
+		my $len = int($length / $kb_per_pixels) + 1;
+
+		$img->setPixel($x, $y, $color);
+	}
+
+	# Open a file for writing 
+	my $filename = "t-" . sprintf("%05d", $fileno) . ".png";
+	print STDERR "write $filename";
+	open(PICTURE, "> $filename") or die("Cannot open file for writing");
+
+	# Make sure we are writing to a binary stream
+	binmode PICTURE;
+	# Convert the image to PNG and print it to the file PICTURE
+	print PICTURE $img->png();
+
+	close PICTURE;
+	print STDERR ".\n";
+	$fileno ++;
+}
+
+# haaa.. this should really be part of Perl :-)
+sub trim
+{
+	my $string = shift;
+	$string =~ s/^\s+//;
+	$string =~ s/\s+$//;
+	return $string;
+}
